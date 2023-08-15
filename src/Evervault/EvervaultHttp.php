a7699f58afab87afd0abcfd786dd38fc2cbaa10e
Jake Grogan <jake.kgrogan@gmail.com>
2023-08-15 09:45:40 +0100

    chore(tokens): handling http responses

--- src/Evervault/EvervaultHttp.php
+++ src/Evervault/EvervaultHttp.php
@@ -96,7 +96,7 @@ class EvervaultHttp {
             throw new EvervaultError('Your API key was invalid. Please verify it matches your API key in the Evervault Dashboard.');
         } else if ($responseCode === 403) {
             throw new EvervaultError('Your API key does not have the required permissions to perform this action. You can update your API key permissions in the Evervault Dashboard.');
-        } else if ($responseCode !== 200 || $responseCode !== 201) {
+        } else if ($responseCode !== 200 && $responseCode !== 201) {
             throw new EvervaultError('There was an error initializing the Evervault SDK. Please try again or contact support@evervault.com for help.');
         } else {
             return json_decode($response);
