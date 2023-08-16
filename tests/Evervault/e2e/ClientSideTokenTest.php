8b80bec9d822d2f276afc7a7b02a2294e6ecc39d
Jeremy Wagemans <jeremy@evervault.com>
2023-08-16 21:03:15 +0100

    improve client side token tests

--- tests/Evervault/e2e/ClientSideTokenTest.php
+++ tests/Evervault/e2e/ClientSideTokenTest.php
@@ -6,15 +6,58 @@ class ClientSideTokenTest extends EndToEndTestCase {
 
     public function testCreateClientSideDecryptToken()
     {
-        $encryptedName = self::$evervaultClient->encrypt('John Doe');
-        $encryptedAge = self::$evervaultClient->encrypt(42);
-        $encryptedIsAlive = self::$evervaultClient->encrypt(true);
-        $response = self::$evervaultClient->createClientSideDecryptToken([
-            'name' => $encryptedName,
-            'age' => $encryptedAge,
-            'isAlive' => $encryptedIsAlive
-        ]);
+        $array = [
+            'name' => 'John Doe',
+            'age' => 42,
+            'isAlive' => true
+        ];
+        $encrypted = self::$evervaultClient->encrypt($array);
+        $response = self::$evervaultClient->createClientSideDecryptToken($encrypted);
         $this->assertNotEmpty($response->id);
         $this->assertNotEmpty($response->token);
+
+        $decrypted = $this->decrypt($response->token, $encrypted);
+        $this->assertEquals($array, $decrypted);
     }
+
+    public function testCreateClientSideDecryptTokenWithExpiry()
+    {
+        $array = [
+            'name' => 'John Doe',
+            'age' => 42,
+            'isAlive' => true
+        ];
+        $encrypted = self::$evervaultClient->encrypt($array);
+        $expiry = time() + 5*60;
+        $response = self::$evervaultClient->createClientSideDecryptToken($encrypted, $expiry);
+        $this->assertNotEmpty($response->id);
+        $this->assertNotEmpty($response->token);
+        $this->assertEquals($expiry * 1000, $response->expiry);
+
+        $decrypted = $this->decrypt($response->token, $encrypted);
+        $this->assertEquals($array, $decrypted);
+
+    }
+
+    private function decrypt($token, $payload) {
+        $url = "https://api.evervault.com/decrypt";
+
+        $ch = curl_init($url);
+        $headers = [
+            "authorization: Token $token",
+            'content-type: application/json'
+        ];
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        if ($response === false) {
+            echo "Curl Error: " . curl_error($ch);
+        } else {
+            echo $response;
+        }
+        curl_close($ch);
+    }
+
 }
