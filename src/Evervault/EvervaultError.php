29f0e21166c5f148d7ea240d0f96fb7f2ab933a8
Jeremy Wagemans <jeremy@evervault.com>
2023-10-16 13:38:25 +0100

    Add changeset

--- src/Evervault/EvervaultError.php
+++ src/Evervault/EvervaultError.php
@@ -3,8 +3,7 @@
 namespace Evervault;
 
 class EvervaultError extends \Exception {
-    public function __construct($message, $code = 0, Exception $previous = null) {
-        // TODO: send error telemetry data to Evervault
+    public function __construct($message, $code = 0, \Exception $previous = null) {
         parent::__construct($message, $code, $previous);
     }
 }
