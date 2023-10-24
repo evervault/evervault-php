dcd3958805b8498d4d9b109858165e9cb7b5a149
Jeremy Wagemans <jeremy@evervault.com>
2023-10-24 14:26:30 +0100

    update e2e credentials

--- tests/Evervault/EndToEnd/EndToEndTestCase.php
+++ tests/Evervault/EndToEnd/EndToEndTestCase.php
@@ -10,12 +10,8 @@ class EndToEndTestCase extends TestCase {
     
     public static function setUpBeforeClass(): void
     {
-        // $appId = getenv('TEST_EV_APP_ID');
-        // $apiKey = getenv('TEST_EV_API_KEY');
-        // self::$evervaultClient = new Evervault($appId, $apiKey);
-        
-        $apiKey = "ev:key:1:7nQ7joJNcLWO2GpWiNPCKmF2fq7hdzEIJIYnyDQ0g8KU0uCEovkMxmCzU5TZwYlxO:VZoLcg:FN997q";
-        $appId = "app_eead1d640d7c";
+        $appId = getenv('TEST_EV_APP_ID');
+        $apiKey = getenv('TEST_EV_API_KEY');
         self::$evervaultClient = new Evervault($appId, $apiKey);
     }
 }
