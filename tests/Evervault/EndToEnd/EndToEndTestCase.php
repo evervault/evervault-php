6047d94a5dbdd578a90ee9c9d074a5cc9e9d26cb
Jeremy Wagemans <jeremy@evervault.com>
2023-10-26 12:15:03 +0100

    clean up

--- tests/Evervault/EndToEnd/EndToEndTestCase.php
+++ tests/Evervault/EndToEnd/EndToEndTestCase.php
@@ -10,12 +10,8 @@ class EndToEndTestCase extends TestCase {
     
     public static function setUpBeforeClass(): void
     {
-        // $appId = getenv('TEST_EV_APP_ID');
-        // $apiKey = getenv('TEST_EV_API_KEY');
-        // self::$evervaultClient = new Evervault($appId, $apiKey);
-
-        // app_3af8435b1a34
-        // ev:key:1:WVcJIsw73WamND0xdgynqoi7p4gR6UBItj0nTrJc5OwtUMNnikcIxSEQgTpGD3Tf:qG32Jo:/lg+bP
-        self::$evervaultClient = new Evervault("app_3af8435b1a34", "ev:key:1:WVcJIsw73WamND0xdgynqoi7p4gR6UBItj0nTrJc5OwtUMNnikcIxSEQgTpGD3Tf:qG32Jo:/lg+bP");
+        $appId = getenv('TEST_EV_APP_ID');
+        $apiKey = getenv('TEST_EV_API_KEY');
+        self::$evervaultClient = new Evervault($appId, $apiKey);
     }
 }
