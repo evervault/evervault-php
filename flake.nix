7389449ca9b2098c6eca72509ddf326bc50ad9b7
Matt Paletta <matt@evervault.com>
2026-08-12 15:16:43 +0100

    Migrate to modern php versions

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Evervault Bindings for PHP";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs:
+        let
+          php = pkgs.php.withExtensions ({ enabled, all }:
+            enabled ++ (with all; [ curl openssl mbstring ]));
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              php
+              php.packages.composer
+              pkgs.nodejs
+            ];
+          };
+        });
+    };
+}
